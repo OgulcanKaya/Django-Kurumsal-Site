19d432d54e835661d33c2efa0661331d0cdea948
Pietras026 <ogulcan.rok1@gmail.com>
2020-04-01 20:51:38 +0300

    3. Video tamamlandı!

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from news.models import category
+
+class categoryAdmin(admin.ModelAdmin):
+   # fields = ['title', 'status']   liste kısıtlama göstermek istenmeyenler için
+
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+admin.site.register(category,categoryAdmin)
