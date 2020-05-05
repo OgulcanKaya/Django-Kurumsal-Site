e2f21d7d4307185600b9a9337b58ed9a6504335a
Pietras026 <ogulcan.rok1@gmail.com>
2020-05-06 02:59:26 +0300

    26. Video Tamamlandı. Profil sayfası yapıldı.

--- KurumsalSite/settings.py
+++ KurumsalSite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Home.apps.HomeConfig',
+    'user.apps.UserConfig',
     'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- KurumsalSite/urls.py
+++ KurumsalSite/urls.py
@@ -23,19 +23,24 @@ from Home import views
 urlpatterns = [
     path('', include('Home.urls')),
     path('home/', include('Home.urls')),
+    path('news/', include('news.urls')),
+    path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
     path('hakkimizda', views.hakkimizda, name='hakkimizda'),
     path('referanslar', views.referanslar, name='referanslar'),
     path('iletisim', views.iletisim, name='iletisim'),
-    path('news/', include('news.urls')),
-    path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>/', views.category_news, name='category_news'),
     path('news/<int:id>/<slug:slug>/', views.news_detail, name='news_detail'),
+
     path('search/', views.news_search, name='news_search'),
     path('search_auto/', views.news_search_auto, name='news_search_auto'),
     path('logout/', views.logout_view, name='logout_view'),
     path('login/', views.login_view, name='login_view'),
     path('register/', views.register_view, name='register_view'),
+
+
 
 ]
 if settings.DEBUG: # new
